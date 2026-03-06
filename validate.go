7e22d1f029939985d65580a044bdb492fbabd390
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-03-06 14:32:53 +0000

    Switching to course solution.

--- validate.go
+++ validate.go
@@ -2,58 +2,51 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
+	"strings"
 )
 
 func handlerValidate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Message string `json:"body"`
+		Body string `json:"body"`
 	}
 
-	type ErrorMsg struct {
-		Error string `json:"errorMsg"`
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		log.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(500)
+		respondWithError(w, http.StatusInternalServerError, "couldn't decode parameters", err)
 		return
 	}
-	// params is a struct with data populated successfully
-	// ...
-	msgLength := len(params.Message)
-	if msgLength > 140 {
-		fmt.Printf("Message is too long:%v\n", msgLength)
-		w.WriteHeader(400)
-		w.Header().Set("Content-Type", "application/json")
-		errorJson := ErrorMsg{
-			Error: "Chirp is too long",
+	const maxChirpLength = 140
+	msgLength := len(params.Body)
+
+	if msgLength > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", err)
+	}
+	badwords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	cleaned := getCleanedBody(params.Body, badwords)
+	// stripDirtyWords(params.Message)
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: cleaned,
+	})
+}
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
 		}
-		errorData, err := json.Marshal(errorJson)
-		if err != nil {
-			fmt.Printf("Error marshalling error data:%v\n", err)
-		}
-		w.Write([]byte(errorData))
-		return
 	}
-	stripDirtyWords(params.Message)
-	type returnVals struct {
-		Valid bool `json:"valid"`
-	}
-	respBody := returnVals{
-		Valid: true,
-	}
-	dat, err := json.Marshal(respBody)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write(dat)
+	cleaned := strings.Join(words, " ")
+	return cleaned
 }
